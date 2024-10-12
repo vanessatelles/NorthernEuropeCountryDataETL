96e50dbbda19f44fd9cb82b92dc6699d670c4624
vanessatelles <tellesvanessa07@gmail.com>
2024-10-12 14:54:12 -0300

    chore: add main file and class CountryData

---
+++ main.py
@@ -0,0 +1,13 @@
+
+class CountryData():
+
+    def __init__(self):
+
+        self.data = {
+            'nation_official_name': list(),
+            'currency_name': list(),
+            'population': list()
+        }
+
+if __name__ == '__main__':
+    print("first code")
