6efb21891fd3f8da836d659eb000edab0cf1a3e3
vanessatelles <tellesvanessa07@gmail.com>
2024-10-21 11:32:39 -0300

    chore: add fetch_data

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+import aiohttp
+import asyncio
+
+async def fetch_data_from_api():
+    """
+    Async function to call the API and fetch data from the endpoint.
+    
+    Returns:
+        dict: Data received from the API as a JSON dictionary.
+    """
+
+    async with aiohttp.ClientSession() as session:
+
+        api_url = 'https://restcountries.com/v3.1/subregion/Northern Europe?fields=name,currencies,population'
+        async with session.get(api_url) as resp:
+            response = await resp.json()
+            return response
 
 class CountryData():
 
