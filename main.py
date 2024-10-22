07eb651d0bcbd4af9580752e1714f38c3bc9e6f1
vanessatelles <tellesvanessa07@gmail.com>
2024-10-22 13:18:32 -0300

    chore: add prepare_database function

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import aiohttp
 import asyncio
+import psycopg2
+from sqlalchemy import create_engine 
+
 
 async def fetch_data_from_api():
     """
@@ -32,6 +35,31 @@ class CountryData():
             self.data['nation_official_name'].append(country['name']['official'])
             self.data['currency_name'].append(country['currencies'][currencies_key]['name'])
             self.data['population'].append(country['population'])
+
+    def prepare_database(table_name):
+   
+        db = create_engine(f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
+        conn = db.connect()
+
+        conn1 =  psycopg2.connect(dbname=DB_NAME,
+                                user=DB_USER,
+                                password=DB_PASSWORD,
+                                host=DB_HOST,
+                                port=DB_PORT)
+        
+        conn1.autocommit = True
+        
+        c = conn1.cursor()
+        
+        c.execute(f'''CREATE TABLE IF NOT EXISTS {table_name} 
+                    (nation_official_name VARCHAR ( 50 ) PRIMARY KEY,
+                    currency_name VARCHAR ( 50 ) NOT NULL,
+                    population INT NOT NULL);''')
+        
+        conn1.commit()
+        conn1.close()
+
+        return conn
 
 if __name__ == '__main__':
     print("first code")
